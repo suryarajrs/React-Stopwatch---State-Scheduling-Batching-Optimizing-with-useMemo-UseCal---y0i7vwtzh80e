20814e1e5c8cd5eac3b49768f8ab4f10d86b80e1
suryarajrs <suryars@outlook.in>
2024-02-26 22:16:57 +0530

    test1

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,51 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import '../styles/App.css';
 const App = () => {
   const startTime = useRef(0);
   const intervalRef = useRef(0);
   const [currentTime, setCurrentTime] = useState(0);
-  const [laps, setLaps] = useState([]);
+  const [laps, setLaps] = useState(null);
+  const [lapSectionVisible, setlapSectionVisible] = useState(false);
+  const [start, setStart] = useState()
 
+  useEffect(()=>{
+    let intervel
+
+    if(start){
+       intervel = setInterval(() => {
+        setCurrentTime((prev)=>prev+.010)
+      },10);
+
+    } 
+
+    return () => clearInterval(intervel);
+
+  },[start])
+
+  function handleLap(){
+    setLaps([...laps,currentTime])
+    setlapSectionVisible(true)
+  }
+   
   return (
     <div id="main">
       <section>
-        <h1 className='seconds-elapsed'>Stopwatch Time</h1>
+        <h1 className='seconds-elapsed'>{currentTime.toFixed(3)}</h1>
         <section className='buttons'>
-          <button className="start-btn">START</button>
-          <button className="stop-btn">STOP</button>
-          <button className="lap-btn">LAP</button>
-          <button className="reset-btn">RESET</button>
+          <button onClick={()=>{setStart(true)}} className="start-btn">START</button>
+          <button onClick={()=>{setStart(false)}} className="stop-btn">STOP</button>
+          <button  onClick={handleLap} className="lap-btn">LAP</button>
+          <button onClick={()=>{setRunning(false) ,setCurrentTime(0),setlapSectionVisible(false),setLaps([])}} className="reset-btn">RESET</button>
         </section>
       </section>
-      <section className='lap-section'>
+      {lapSectionVisible && <section className='lap-section'>
         <h2>Laps</h2>
         <section className='laps'>
           <p>lap</p>
           <p>lap</p>
           <p>lap</p>
         </section>
-      </section>
+      </section>}
     </div>
   )
 }
